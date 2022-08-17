9f109dee92b41ac70839531005e7d8a9bc50bac0
sharow yeh <yuyu.yei@hotmail.com>
2022-08-17 16:31:34 +0800

    upgrade v143, add ini customization, add ROI info

--- sample-app/MatWidget.h
+++ sample-app/MatWidget.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #define GLFW_EXPOSE_NATIVE_WIN32
+/* for 2D texture */
 #include <GL/glew.h>
+
 #include "imgui/imgui.h"
 #include "imgui/imgui_internal.h"
 #include <GLFW/glfw3.h>
@@ -24,6 +26,15 @@
 #include <mutex>
 
 namespace GLUI {
+
+	struct Roi {
+		float x = 0;
+		float y = 0;
+		float width = 0;
+		float height = 0;
+		std::string id;
+	};
+
 	class MatWidget {
 	private:
 		GLuint texture;
@@ -34,12 +45,14 @@ namespace GLUI {
 		const char* widget_name;
 		ImGuiWindowFlags widget_flags;
 		std::mutex mtx;
+		std::vector<Roi> rois;
+		Roi drag_roi;
 	public:
 		bool IsShow = true;
 		bool IsDraw = true;
 	public:
 		MatWidget(const char* name, ImGuiWindowFlags flags) {
-			widget_init = true;
+			widget_init = false;
 			widget_name = name;
 			widget_flags = flags;
 
@@ -52,6 +65,15 @@ namespace GLUI {
 
 			image_width = 0;
 			image_height = 0;
+
+			for (int i = 0; i < 3; i++) {
+				Roi roi;
+				roi.x = i * 50;
+				roi.width = 30 + (i * 5);
+				roi.height = 30 + (i * 5);
+				roi.id = std::string("roi") + std::to_string(i);
+				rois.push_back(roi);
+			}
 		};
 
 		/* code: cv::ColorConversionCodes, transform to rgba for opengl texture */
@@ -59,6 +81,85 @@ namespace GLUI {
 			mtx.lock();
 			cv::cvtColor(mat, image, code);
 			mtx.unlock();
+		}
+
+		// Gets back to previous line and continue with horizontal layout
+		//      offset_from_start_x == 0 : follow right after previous item
+		//      offset_from_start_x != 0 : align to specified x position (relative to window/group left)
+		//      spacing_w < 0            : use default spacing if pos_x == 0, no spacing if pos_x != 0
+		//      spacing_w >= 0           : enforce spacing amount
+		void SameLoc(float offset_from_start_x, float offset_from_start_y)
+		{
+			ImGuiWindow* window = ImGui::GetCurrentWindow();
+			if (window->SkipItems)
+				return;
+
+			ImGuiContext& g = *GImGui;
+			if (offset_from_start_x != 0.0f)
+			{
+				window->DC.CursorPos.x = window->DC.CursorStartPos.x + offset_from_start_x;
+				window->DC.CursorPos.y = window->DC.CursorPosPrevLine.y;
+			}
+			else
+			{
+				window->DC.CursorPos.x = window->DC.CursorStartPos.x;
+				window->DC.CursorPos.y = window->DC.CursorPosPrevLine.y;
+			}
+			window->DC.CurrLineSize = window->DC.PrevLineSize;
+			window->DC.CurrLineTextBaseOffset = window->DC.PrevLineTextBaseOffset;
+		}
+
+		void RenderROI() {
+			
+			ImGuiIO& io = ImGui::GetIO();
+
+			for (int i = 0; i < rois.size(); i++) {
+				ImGui::PushID(i);
+				
+				SameLoc(rois[i].x, rois[i].y);
+				
+				ImGui::BeginChildFrame(ImGui::GetID(rois[i].id.c_str()), ImVec2(rois[i].width, rois[i].height), true);
+
+				if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None | ImGuiDragDropFlags_SourceAllowNullID))
+				{
+					// Set payload to carry the index of our item (could be anything)
+					ImGui::SetDragDropPayload("ROI_INDEX", &i, sizeof(int));
+					drag_roi = rois[i];
+					//TODO: save origin x/y to payload?
+
+					// Display preview (could be anything, e.g. when dragging an image we could decide to display
+					// the filename and a small preview of the image, etc.)
+					ImGui::Text("Move %s pos %g %g", rois[i].id.c_str(), io.MousePos.x, io.MousePos.y);
+					ImGui::EndDragDropSource();
+				}
+
+				ImGui::EndChildFrame();
+
+				ImGui::PopID();
+			}
+			if (ImGui::BeginDragDropTarget())
+			{
+				//TODO: read origin x/y calc offset apply to roi list
+				if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("ROI_INDEX"))
+				{
+					IM_ASSERT(payload->DataSize == sizeof(int));
+					int payload_n = *(const int*)payload->Data;
+					//std::swap(rois[i], rois[payload_n]);
+				}
+				ImGui::EndDragDropTarget();
+			}
+			//SameLoc(0, 0);
+			//ImGui::BeginChildFrame(ImGui::GetID("mychild"), ImVec2(40, 40), true);
+			//if (ImGui::BeginDragDropSource(/*ImGuiDragDropFlags_SourceAllowNullID*/)) {
+			//	auto myid = ImGui::GetID("mychild");
+			//	ImGui::SetDragDropPayload("what-is-type-for", &myid, sizeof(ImGuiID));
+			//	ImGui::Text("This is a drag and drop source");
+			//	ImGui::EndDragDropSource();
+			//}
+			//ImGui::EndChildFrame();
+			//SameLoc(40, 40);
+			//ImGui::BeginChildFrame(ImGui::GetID("mychild2"), ImVec2(50, 50), true);
+			//ImGui::EndChildFrame();
 		}
 
 		void Render() {
@@ -74,12 +175,15 @@ namespace GLUI {
 			mtx.unlock();
 
 			/* UI part with opengl texture */
-			if (widget_init) {
+			if (widget_init == false) {
+				// reset widget position when first time shows up
 				ImGui::SetNextWindowPos(ImVec2(0, 0));
-				widget_init = false;
+				widget_init = true;
 			}
 			ImGui::Begin(widget_name, &IsShow, widget_flags);
 			ImGui::Image(reinterpret_cast<void*>(static_cast<intptr_t>(texture)), ImVec2(image_width, image_height));
+			
+			RenderROI();
 			ImGui::End();
 		}
 	};
